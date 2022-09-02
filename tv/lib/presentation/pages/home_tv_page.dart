636212c8dcc01c979efe2489296aa1653f7d0bab
Moch. Fariz Al Hazmi <moch.fariz@bukalapak.com>
2022-09-03 05:19:53 +0700

    add tv module

---
+++ tv/lib/presentation/pages/home_tv_page.dart
@@ -0,0 +1,207 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:core/utils/constants.dart';
+import 'package:core/utils/state_enum.dart';
+import 'package:core/styles/text_styles.dart';
+import 'package:core/utils/routes.dart';
+import 'package:core/domain/entities/tv.dart';
+import 'package:tv/presentation/provider/tv_list_notifier.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class HomeTVPage extends StatefulWidget {
+  static const ROUTE_NAME = '/tvshow';
+
+  @override
+  _HomeTVPageState createState() => _HomeTVPageState();
+}
+
+class _HomeTVPageState extends State<HomeTVPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(
+            () => Provider.of<TVListNotifier>(context, listen: false)
+          ..fetchNowPlayingTVs()
+          ..fetchPopularTVs()
+          ..fetchTopRatedTVs());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/circle-g.png'),
+              ),
+              accountName: Text('Ditonton'),
+              accountEmail: Text('ditonton@dicoding.com'),
+            ),
+            ListTile(
+              leading: Icon(Icons.movie),
+              title: Text('Movies'),
+              onTap: () {
+                Navigator.pushNamed(context, HOME_ROUTE);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.tv),
+              title: Text('TV Show'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.save_alt),
+              title: Text('Watchlist'),
+              onTap: () {
+                Navigator.pushNamed(context, WATCHLIST_ROUTE);
+              },
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.pushNamed(context, ABOUT_ROUTE);
+              },
+              leading: Icon(Icons.info_outline),
+              title: Text('About'),
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: Text('Ditonton'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.pushNamed(context, TV_SEARCH_ROUTE);
+            },
+            icon: Icon(Icons.search),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Now Playing',
+                style: kHeading6,
+              ),
+              Consumer<TVListNotifier>(builder: (context, data, child) {
+                final state = data.nowPlayingTVsState;
+                if (state == RequestState.Loading) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (state == RequestState.Loaded) {
+                  return TVList(data.nowPlayingTVs);
+                } else {
+                  return Text('Failed');
+                }
+              }),
+              _buildSubHeading(
+                title: 'Popular',
+                onTap: () => Navigator.pushNamed(context, TVS_POPULAR_ROUTE),
+              ),
+              Consumer<TVListNotifier>(builder: (context, data, child) {
+                final state = data.popularTVsState;
+                if (state == RequestState.Loading) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (state == RequestState.Loaded) {
+                  return TVList(data.popularTVs);
+                } else {
+                  return Text('Failed');
+                }
+              }),
+              _buildSubHeading(
+                title: 'Top Rated',
+                onTap: () => Navigator.pushNamed(context, TVS_TOP_RATED_ROUTE),
+              ),
+              Consumer<TVListNotifier>(builder: (context, data, child) {
+                final state = data.topRatedTVsState;
+                if (state == RequestState.Loading) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (state == RequestState.Loaded) {
+                  return TVList(data.topRatedTVs);
+                } else {
+                  return Text('Failed');
+                }
+              }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row _buildSubHeading({required String title, required Function() onTap}) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          style: kHeading6,
+        ),
+        InkWell(
+          onTap: onTap,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [Text('See More'), Icon(Icons.arrow_forward_ios)],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TVList extends StatelessWidget {
+  final List<TV> tvs;
+
+  TVList(this.tvs);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          final tv = tvs[index];
+          return Container(
+            padding: const EdgeInsets.all(8),
+            child: InkWell(
+              onTap: () {
+                Navigator.pushNamed(
+                  context,
+                  TV_DETAIL_ROUTE,
+                  arguments: tv.id,
+                );
+              },
+              child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(16)),
+                child: CachedNetworkImage(
+                  imageUrl: '$BASE_IMAGE_URL${tv.posterPath}',
+                  placeholder: (context, url) => Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
+                ),
+              ),
+            ),
+          );
+        },
+        itemCount: tvs.length,
+      ),
+    );
+  }
+}
---
+++ tv/test/domain/usecases/get_tv_detail_test.dart
@@ -0,0 +1,29 @@
+import 'package:dartz/dartz.dart';
+import 'package:tv/domain/usecases/get_tv_detail.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../../core/test/dummy_data/dummy_objects.dart';
+import '../../../../core/test/helpers/test_helper.mocks.dart';
+
+void main() {
+  late GetTVDetail usecase;
+  late MockTVRepository mockTVRepository;
+
+  setUp(() {
+    mockTVRepository = MockTVRepository();
+    usecase = GetTVDetail(mockTVRepository);
+  });
+
+  final tId = 1;
+
+  test('should get tv detail from the repository', () async {
+    // arrange
+    when(mockTVRepository.getTVDetail(tId))
+        .thenAnswer((_) async => Right(mockedTVDetail));
+    // act
+    final result = await usecase.execute(tId);
+    // assert
+    expect(result, Right(mockedTVDetail));
+  });
+}
