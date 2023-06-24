f6655f127d18e25fb2f57d9ebcb4aa641e09f698
Moch. Fariz Al Hazmi <mochfarizalhazmi@gmail.com>
2023-06-24 18:01:50 +0700

    Fixing some error

--- tv/lib/presentation/pages/home_tv_page.dart
+++ tv/lib/presentation/pages/home_tv_page.dart
@@ -182,10 +182,10 @@ class _HomeTVPageState extends State<HomeTVPage> {
         ),
         InkWell(
           onTap: onTap,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
+          child: const Padding(
+            padding: EdgeInsets.all(8.0),
             child: Row(
-              children: const [Text('See More'), Icon(Icons.arrow_forward_ios)],
+              children: [Text('See More'), Icon(Icons.arrow_forward_ios)],
             ),
           ),
         ),
